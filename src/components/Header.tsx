2cef00421adc711d6135509a6412de5d7269f9f4
Andre Ofuji <30282153+aofuji@users.noreply.github.com>
2025-02-24 21:14:00 -0300

    Add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
+
 const Header = () => {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="bg-white mx-auto p-6 shadow-lg w-full">
+      <Image src="/img/logo.png" alt="" width={71} height={50} />
     </header>
   );
-}
+};
 
 export default Header;
