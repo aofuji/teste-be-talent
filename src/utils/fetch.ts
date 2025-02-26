525f29065d3863564c4d94b7e3878a0a06afe2c3
Andre Ofuji <30282153+aofuji@users.noreply.github.com>
2025-02-26 08:44:27 -0300

    Adjust Error

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,7 +1,17 @@
 import { Employee } from "@/interfaces/employees";
 
 const fetcher = (url: string): Promise<Employee[]> => {
-  return fetch(url).then((res) => res.json());
+  return fetch(url)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Resposta da rede não foi boa");
+      }
+      return res.json();
+    })
+    .catch((error) => {
+      console.error("Houve um problema com sua operação de busca:", error);
+      return [];
+    });
 };
 
 export { fetcher };
