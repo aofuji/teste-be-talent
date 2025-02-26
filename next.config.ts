007bea4c18d9d47c5eb3d954727373b419ef6c02
Andre Ofuji <30282153+aofuji@users.noreply.github.com>
2025-02-26 09:31:00 -0300

    FIx

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["img.favpng.com","e7.pngegg.com","www.clipartmax.com"],    
-  },  
+    domains: ["img.favpng.com", "e7.pngegg.com", "www.clipartmax.com"],
+  },
 };
 
 export default nextConfig;
